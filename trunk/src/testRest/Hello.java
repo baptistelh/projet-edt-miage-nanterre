80f7696da4959ca5b6dbcd203b94e463cd98343b
flegaludec@gmail.com <flegaludec@gmail.com@638a33bc-f929-9441-b019-cb0cb3681f00>
2012-06-13 22:54:26 +0000

    ajout classes rest pour parser un objet en json

--- trunk/src/testRest/Hello.java
+++ trunk/src/testRest/Hello.java
@@ -17,34 +17,35 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class Hello {
 
-	// This method is called if TEXT_PLAIN is request
-	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String sayPlainTextHello() {
-		return "Hello Jersey";
-	}
+//	// This method is called if TEXT_PLAIN is request
+//	@GET
+//	@Produces(MediaType.TEXT_PLAIN)
+//	public String sayPlainTextHello() {
+//		return "Hello Jersey";
+//	}
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Event sayJsonHello() {
 		Event e1 = new Event("flo","bat");
+		
 		return e1;
 	}
 
-	// This method is called if XML is request
-	@GET
-	@Produces(MediaType.TEXT_XML)
-	public String sayXMLHello() {
-		return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
-	}
+//	// This method is called if XML is request
+//	@GET
+//	@Produces(MediaType.TEXT_XML)
+//	public String sayXMLHello() {
+//		return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
+//	}
 
-	// This method is called if HTML is request
-	@GET
-	@Produces(MediaType.TEXT_HTML)
-	public String sayHtmlHello() {
-		return "<html> " + "<title>" + "Hello Jersey" + "</title>"
-				+ "<body><h1>" + "Hello Jersey" + "</body></h1>" + "</html> ";
-	}
+//	// This method is called if HTML is request
+//	@GET
+//	@Produces(MediaType.TEXT_HTML)
+//	public String sayHtmlHello() {
+//		return "<html> " + "<title>" + "Hello Jersey" + "</title>"
+//				+ "<body><h1>" + "Hello Jersey" + "</body></h1>" + "</html> ";
+//	}
 
 }
 			
