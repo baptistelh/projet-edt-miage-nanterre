485d50df5ca0ca4d8a7d38120013a9c2f4edf166
onur.mia@gmail.com <onur.mia@gmail.com@638a33bc-f929-9441-b019-cb0cb3681f00>
2012-06-18 09:41:05 +0000

    début implementation setCreneau pour enseignant; Implementation verification chevauchement crenaux et depassement journée, générique à tous les créneau Déclaration méthodes ODBC à implémenter

--- src/beansUtilisation/EnseignantEDT.java
+++ src/beansUtilisation/EnseignantEDT.java
@@ -1,9 +1,37 @@
 package beansUtilisation;
 
-public class EnseignantEDT {
-	
-	
-	
-	
+import beans.*;
 
+import java.sql.Connection;
+import java.util.Enumeration;
+import java.util.GregorianCalendar;
+import java.util.Hashtable;
+import java.util.List;
+
+
+
+public class EnseignantEDT {
+
+	public void setCreneau(List<Creneau> newCreneaux, Enseignant e) {
+
+		// Connection à la base et récupération de tous les créneaux de
+		// l'enseignant en paramètre
+		String login = "";
+		String password = "";
+		String url = "";
+		Connection connection = ODBC_onur.getConnectiontoDB(login, password,
+				url);
+		List<Creneau> creneauxRegistred = ODBC_onur.getCreneaux(connection, e);
+
+		// Comparaison chevauchement creneaux;
+		boolean chevauche = verificationCreneaux.chevauchementCreneaux(
+				creneauxRegistred, newCreneaux);
+		List<Creneau> merge = creneauxRegistred;
+		merge.addAll(newCreneaux);
+
+		boolean depassementjournee = verificationCreneaux.depassementJournee(merge);
+
+	}
+
+	
 }
---
+++ src/beansUtilisation/ODBC_onur.java
@@ -0,0 +1,34 @@
+package beansUtilisation;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.List;
+import beans.*;
+
+public class ODBC_onur {
+
+	public static Connection getConnectiontoDB(String login, String password,
+			String url) {
+
+		Connection connection = null;
+
+		try {
+			connection = DriverManager.getConnection(url, login, password);
+			// interaction avec la base
+		} catch (SQLException sqle) {
+			// cf Comment gérer les erreurs ?
+		} 
+		return connection;
+		}
+	
+	//recupere tous les creneaux d'un enseigant
+	public static List <Creneau> getCreneaux (Connection c, Enseignant e)(){	
+	}
+	
+	//enregistre creneaux d'un enseignant. L'idee est de tout écrasé dans la base pour l'enseignant donné et de réecire. 
+	public static boolean setCreneau
+
+
+}
