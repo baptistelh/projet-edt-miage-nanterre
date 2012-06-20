e6892760c8dedaaa25ce29f60c4c01081abd5c28
flegaludec@gmail.com <flegaludec@gmail.com@638a33bc-f929-9441-b019-cb0cb3681f00>
2012-06-20 14:03:09 +0000

    Modifications des types pour le web service (ajout de l'EC, de l'enseignant,...)

--- trunk/src/webservice/GetScheduleWS.java
+++ trunk/src/webservice/GetScheduleWS.java
@@ -43,32 +43,32 @@ public class GetScheduleWS {
 		JSONConfiguration.natural().rootUnwrapping(false).build();
 		
 		Calendar c = new Calendar();
+		
 		Enseignant e1=new Enseignant(1,"Aragou", "muriel", "1 avenue du meilleur des tuteurs" , "0143855907",new GregorianCalendar(1960,10, 10), "logAragou", "pwdAragou");	
 		Salle s1=new Salle("210A", 50, "enferNum1");
 		Formation F1=new Formation();
 		UE ue1=new UE(1,F1);
-		EC ec1= new EC(1, ue1, F1);
-		Type type1 = new Type(1, "TD"); 
-		Creneau cLundi = new Creneau(e1, s1, ec1,type1, "09:00", 120);
+		EC ec1= new EC(1, ue1);
+		Type type1 = new Type(1, "TD");
 		Jours j1=new Jours();
-		j1.setDate(new GregorianCalendar(2012, 05, 19));
-		cLundi.setDateCreneau(j1);
+		j1.setDateDuJour(new GregorianCalendar(2012, 05, 19));
+		Creneau cLundi = new Creneau(e1, s1, ec1, type1, j1, "09:00");
+		cLundi.setDuree(120);
 		
 		Event e = new Event(cLundi);
-		
 		c.addEvt(e);
 		
 		Enseignant e2=new Enseignant(1,"Giroud", "Olivier", "1 avenue des coupeurs d'orange" , "0143855908",new GregorianCalendar(1960,10, 10), "logGiroud", "pwdGiroud");	
 		Salle s2=new Salle("210A", 50, "enferNum2");
 		Formation F2=new Formation();
 		UE ue2=new UE(1,F2);
-		EC ec2= new EC(1, ue2, F2);
+		EC ec2= new EC(1, ue2);
 		Type type2 = new Type(1, "TD"); 
-		Creneau cMardi = new Creneau(e2, s2, ec2,type2, "11:00", 120);
 		Jours j2=new Jours();
-		j2.setDate(new GregorianCalendar(2012, 05, 20));
-		cMardi.setDateCreneau(j2);	
-		
+		j2.setDateDuJour(new GregorianCalendar(2012, 05, 20));
+		Creneau cMardi = new Creneau(e2, s2, ec2, type2, j2, "11:00");
+		cMardi.setDuree(180);
+	
 		e = new Event(cMardi);
 		c.addEvt(e);
 		
