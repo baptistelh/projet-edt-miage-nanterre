3b9cf36c333e05b6ab5aa5b6a29f44944ae5cad5
flegaludec@gmail.com <flegaludec@gmail.com@638a33bc-f929-9441-b019-cb0cb3681f00>
2012-06-21 10:17:45 +0000

    Ajout des DAO nécessaires (non implementées)

---
+++ src/dao/EnseignantDAO.java
@@ -0,0 +1,31 @@
+package dao;
+
+import beans.*;
+
+public class EnseignantDAO extends DAO<Enseignant> {
+
+	@Override
+	public Enseignant create(Enseignant arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Enseignant arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Enseignant find(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Enseignant update(Enseignant arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/dao/TypeDAO.java
@@ -0,0 +1,31 @@
+package dao;
+
+import beans.*;
+
+public class TypeDAO extends DAO<Type> {
+
+	@Override
+	public Type create(Type arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Type arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Type find(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type update(Type arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
