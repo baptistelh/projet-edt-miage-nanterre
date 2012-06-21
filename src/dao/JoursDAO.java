c5697eb2b685a2e6e81635e962d4e44f7eedb85b
flegaludec@gmail.com <flegaludec@gmail.com@638a33bc-f929-9441-b019-cb0cb3681f00>
2012-06-21 12:25:49 +0000

    Méthodes DAO Jours complétée. Il manque encore le load des listes.  Modifications mineures dans CréneauDAO pour la gestion du find de joursDAO

--- src/dao/JoursDAO.java
+++ src/dao/JoursDAO.java
@@ -4,110 +4,57 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import beans.*;
 
 public class JoursDAO extends DAO<Jours> {
-	
+
 	public static String TABLE = "JOURS";
-	
+
 	public static boolean debug = true;
-	
+
 	public List<Jours> findAll() {
-		List <Jours> listeJours= new ArrayList<Jours>();
+		List<Jours> listeJours = new ArrayList<Jours>();
 		Jours obj = null;
 		try {
-			Statement request = this.connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
-			ResultSet result = request.executeQuery("SELECT * FROM " + JoursDAO.TABLE);
+			Statement request = this.connect.createStatement(
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_UPDATABLE);
+			ResultSet result = request.executeQuery("SELECT * FROM "
+					+ JoursDAO.TABLE);
 
-			while (result.next()){
-				
-				EnseignantDAO enseignantDAO= new EnseignantDAO();
-				SalleDAO salleDAO= new SalleDAO();
-				ECDAO ecDAO= new ECDAO();
-				TypeDAO typeDAO= new TypeDAO();
-				JoursDAO joursDAO= new JoursDAO();
-				
-
+			while (result.next()) {
 				obj = new Jours();
-				obj.setDateDuJour(result.getDate());
-				
-				obj.setMonEnseignant(enseignantDAO.find(result.getInt("NO_ENSEIGNANT")));
-				obj.setMaSalle(salleDAO.find(result.getInt("NO_SALLE")));
-				obj.setMonEC(ecDAO.find(result.getInt("NO_EC"), result.getInt("NO_UE"), result.getInt("NO_FORMATION")));
-				obj.setMonType(typeDAO.find(result.getInt("NO_TYPE")));
-				obj.setDateCreneau(joursDAO.find(result.getDate("DATE_CRENEAU")));
-				obj.setDuree(result.getInt("DUREE_CRENEAU"));
-				obj.setHoraire(result.getString("DUREE_CRENEAU"));
-				
-				listeCreneaux.add(obj);
-				
+				obj.setDateDuJour(DAO.dateFromOracleToJava(result
+						.getDate("DATE_JOUR")));
+				listeJours.add(obj);
 			}
 			request.close();
-	    } catch (SQLException e) {
-	            e.printStackTrace();
-	    }
-	    return listeCreneaux;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return listeJours;
 	}
 
-		@Override
+	@Override
 	public Jours create(Jours obj) {
-			try {
-					Statement request = this.connect.createStatement();
-					String requete = "INSERT INTO "+ FormationDAO.TABLE+" (NO_ENSEIGNANT, " +
-												                            "NO_SALLE, " +
-												                            "NO_EC, " +
-												                            "NO_UE, " +
-												                            "NO_FORMATION, " +
-												                            "NO_TYPE, " +
-												                            "DATE_CRENEAU, " +
-												                            "HORAIRE_CRENEAU, " +
-												                            "DUREE_CRENEAU) " +
-										"VALUES ("+
-												                            obj.getMonEnseignant().getNumeroEnseignant() +","
-												                    + "'" + obj.getMaSalle().getNumeroSalle() +"',"
-												                          + obj.getMonEC().getNumeroEC()+","
-												                          + obj.getMonEC().getMonUE().getNumeroUE()+","
-												                          + obj.getMonEC().getMonUE().getMaFormation().getNumeroFormation()+","
-												                          + obj.getMonType().getNumeroType()+",'"
-												                          + DAO.dateFromJavaToOracle(obj.getDateCreneau().getDateDuJour()) +"',"
-												                    + "'" + obj.getHoraire() +"',"
-												                          + obj.getDuree() + ")";
-					
-						request.executeUpdate(requete);
-					
-					if(debug) System.out.println(requete);		                                                 
-				
-					request.close();
-				/*	if(!obj.getMesUE().isEmpty()){
-						for(int i=0;i<obj.getMesUE().size();i++){				
-							UEDAO ueDAO = new UEDAO();
-							if(ueDAO.find(obj.getMesUE().get(i).getNumeroUE()) ==null){
-								ueDAO.create(obj.getMesUE().get(i));
-							}
-							else{
-								//on met a jour
-							}
-						}
-					}*/
-				/*	if(!obj.getMesPromotions().isEmpty()){
-						for(int i=0;i<obj.getMesPromotions().size();i++){				
-							PromotionDAO promoDAO = new PromotionDAO();
-							if(promoDAO.find(obj.getMesPromotions().get(i).getNumeroPromotion()) ==null){
-								promoDAO.create(obj.getMesPromotions().get(i));
-							}
-							else{
-								//on met a jour
-							}
-						}
-					}*/
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			return obj;
-
+		try {
+			Statement request = this.connect.createStatement();
+			String requete = "INSERT INTO " + FormationDAO.TABLE
+					+ " (DATE_JOUR)" + "VALUES ("
+					+ DAO.dateFromJavaToOracle(obj.getDateDuJour()) + ")";
+			request.executeUpdate(requete);
+			if (debug)
+				System.out.println(requete);
+			request.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return obj;
 	}
 
 	@Override
@@ -118,53 +65,18 @@ public class JoursDAO extends DAO<Jours> {
 
 	@Override
 	public void delete(Jours obj) {
-			try {
-				Statement request = this.connect.createStatement();
-				String requete = "DELETE FROM " + CreneauDAO.TABLE +" WHERE NO_ENSEIGNANT = " + obj.getMonEnseignant().getNumeroEnseignant() +
-		                        " AND   NO_SALLE = " +  obj.getMaSalle().getNumeroSalle() +
-		                        " AND   NO_EC = " + obj.getMonEC().getNumeroEC() +
-		                        " AND   NO_UE = " + obj.getMonEC().getMonUE().getNumeroUE() +
-		                        " AND   NO_FORMATION = " + obj.getMonEC().getMonUE().getMaFormation().getNumeroFormation() +
-		                        " AND   NO_TYPE = " + obj.getMonType().getNumeroType() +
-		                        " AND   DATE_CRENEAU = " + DAO.dateFromJavaToOracle(obj.getDateCreneau().getDateDuJour()) + ";";
-				request.executeUpdate(requete);
-				
-				request.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		
-	}
-	
-	public void loadMesUE(Formation obj) {
-		int id = obj.getNumeroFormation();
 		try {
-			Statement request = this.connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
-			ResultSet result = request.executeQuery("SELECT * FROM " + UEDAO.TABLE + " WHERE NO_FORMATION = " + id);
+			Statement request = this.connect.createStatement();
+			String requete = "DELETE FROM " + CreneauDAO.TABLE
+					+ " WHERE DATE_JOUR = "
+					+ DAO.dateFromJavaToOracle(obj.getDateDuJour()) + ";";
+			request.executeUpdate(requete);
+			request.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-			while(result.next()){
-				UE ue = new UE(result.getInt("NO_UE"), obj);
-				obj.getMesUE().add(ue);
-			}
-	    } catch (SQLException e) {
-	            e.printStackTrace();
-	    }
-	}
-	
-	public void loadMesPromotions(Creneau obj) {
-		int id = obj.getNumeroFormation();
-		try {
-			Statement request = this.connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
-			ResultSet result = request.executeQuery("SELECT * FROM " + PromotionDAO.TABLE + " WHERE NO_FORMATION = " + id);
-
-			while(result.next()){
-				Promotion p = new Promotion(result.getInt("NO_PROMOTION"));
-				obj.getMesPromotions().add(p);
-			}
-	    } catch (SQLException e) {
-	            e.printStackTrace();
-	    }
 	}
 
 	@Override
@@ -173,4 +85,21 @@ public class JoursDAO extends DAO<Jours> {
 		return null;
 	}
 
+	public Jours find(GregorianCalendar date) {
+		Jours j = new Jours();
+		try {
+
+			Statement request = this.connect.createStatement(
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_UPDATABLE);
+			ResultSet result = request.executeQuery("SELECT * FROM "
+					+ JoursDAO.TABLE + "WHERE DATE_JOUR = "
+					+ DAO.dateFromJavaToOracle(date) + ";");
+
+			j = new Jours(DAO.dateFromOracleToJava(result.getDate("DATE_JOUR")));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return j;
+	}
 }
