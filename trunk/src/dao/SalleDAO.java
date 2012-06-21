30f38c586c4ff96adb6994aafd2b2d3db05be8bb
flegaludec@gmail.com <flegaludec@gmail.com@638a33bc-f929-9441-b019-cb0cb3681f00>
2012-06-21 10:17:45 +0000

    Ajout des DAO nécessaires (non implementées)

---
+++ trunk/src/dao/SalleDAO.java
@@ -0,0 +1,31 @@
+package dao;
+
+import beans.*;
+
+public class SalleDAO extends DAO<Salle> {
+
+	@Override
+	public Salle create(Salle arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Salle arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Salle find(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Salle update(Salle arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
