26c35807eb13c37c211c6cc6877ded2bcb758332
baptiste.lehouarner@gmail.com <baptiste.lehouarner@gmail.com@638a33bc-f929-9441-b019-cb0cb3681f00>
2012-06-21 10:39:06 +0000

    Retour à la vue 7 jours

--- WebContent/src/view/Week.js
+++ WebContent/src/view/Week.js
@@ -15,32 +15,5 @@ Ext.define('Ext.calendar.view.Week', {
      * @cfg {Number} dayCount
      * The number of days to display in the view (defaults to 7)
      */
-    // These settings create a fixed weekday view. 
-    // This view will only show Mon-Fri.
-    dayCount: 5,
-    // Always start the view on Monday
-    startDay: 1,
-    startDayIsStatic: true,
-    
-    // NOTE: the configs below apply to any DayView or WeekView. If you wanted all day
-    // and week views to share these same settings, you could simply pass these configs
-    // in the general viewCfg. Any views that do not use them will ignore them. They are
-    // only on this view in this sample to demonstrate how they can be easily customized per view.
-    
-    // Hide the half-hour marker line
-    showHourSeparator: false,
-    // Start the view at 6:00
-    viewStartHour: 6,
-    // End the view at 8:00pm / 20:00
-    viewEndHour: 20,
-    // Default the scroll position on load to 8:00 if the body is overflowed
-    scrollStartHour: 8,
-    // Customize the hour (and event) heights. See the docs for details on setting this.
-    // This example will be double-height (the default is 42)
-    hourHeight: 84,
-    // Allow drag-drop, drag-create and resize of events in 10-minute increments
-    ddIncrement: 10,
-    // Since the hour blocks are double-height, we can shorten the minimum event display 
-    // height to match the ddIncrement
-    minEventDisplayMinutes: 10
+    dayCount: 7
 });
