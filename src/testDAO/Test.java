95ff55f9bae09cd965d77d54599a3e491ad112ad
baptiste.lehouarner@gmail.com <baptiste.lehouarner@gmail.com@638a33bc-f929-9441-b019-cb0cb3681f00>
2012-06-26 20:05:29 +0000

    Retrait de code temporaire (remplacement commentaire)

--- src/testDAO/Test.java
+++ src/testDAO/Test.java
@@ -1,15 +1,9 @@
 package testDAO;
 
-import daoEdt.SalleDAO;
-
 public class Test {
 	
 	public static void main(String[] args) {
-		SalleDAO sd = new SalleDAO();
-		
-		
-		
-		
+		//Instanciation du DAO de la salle
+			
 	}
-
 }
