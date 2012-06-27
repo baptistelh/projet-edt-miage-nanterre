28af885e6f5820822c71d8ecb5e3d65522a7a0f5
baptiste.lehouarner@gmail.com <baptiste.lehouarner@gmail.com@638a33bc-f929-9441-b019-cb0cb3681f00>
2012-06-27 17:09:11 +0000

    Changement nom classe par rapport aux normes de nomages Java

---
+++ src/utilitaire/Test.java
@@ -0,0 +1,19 @@
+package utilitaire;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+
+public class Test {
+	public static void main(String[] args) {
+		GenererJourFeries gen = new GenererJourFeries();
+		SimpleDateFormat si = new SimpleDateFormat(("dd/MM/yyyy"));
+		List<GregorianCalendar> joursFeries = gen.genererJoursFeries();
+        for(GregorianCalendar g : joursFeries) {
+        	System.out.println(si.format(g.getTime()));
+        }
+		
+	}
+
+}
