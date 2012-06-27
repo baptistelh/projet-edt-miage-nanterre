cb50201af321ab38966ac47067950599541bf3f7
flegaludec@gmail.com <flegaludec@gmail.com@638a33bc-f929-9441-b019-cb0cb3681f00>
2012-06-27 15:54:44 +0000

    Ajout des bouchons pour les DAO utlisées par le projet

---
+++ src/testDAO/SalleDAO.java
@@ -0,0 +1,54 @@
+package testDAO;
+import java.util.ArrayList;
+import java.util.List;
+
+import beans.*;
+
+public class SalleDAO {
+	private static List<Salle> list = new ArrayList<Salle>();
+	
+	public Salle create(Salle obj) {
+		SalleDAO.list.add(obj);
+		return obj;
+	}
+
+	public void delete(Salle obj) {
+		SalleDAO.list.remove(obj);
+	}
+
+	public List<Salle> findAll() {
+		return list;
+	}
+
+	public Salle update(Salle obj) {
+		for(Salle s : SalleDAO.list) {
+			if(s.getNumeroSalle() == obj.getNumeroSalle())
+				SalleDAO.list.remove(s);
+		}
+		SalleDAO.list.add(obj);		
+		return obj;
+	}
+
+	
+	
+	public Salle find(String no_salle) {
+		for(Salle s : SalleDAO.list) {
+			if(s.getNumeroSalle() == no_salle)
+				return s;
+		}
+		return null;
+	}
+
+	public Salle find(int arg0) {
+		return null;
+	}
+	
+	public void loadMesCreneaux(Salle obj) {
+	
+	}
+	
+	public void loadMesReunions(Salle obj) {
+		
+	}
+
+}
