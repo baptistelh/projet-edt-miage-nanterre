14141013318bcf875dd4a518f8bbd1867963d893
baptiste.lehouarner@gmail.com <baptiste.lehouarner@gmail.com@638a33bc-f929-9441-b019-cb0cb3681f00>
2012-06-27 20:48:03 +0000

    Corrections violations

--- trunk/src/beansutilisation/ModeleCreaneau.java
+++ trunk/src/beansutilisation/ModeleCreaneau.java
@@ -1,11 +1,11 @@
 package beansutilisation;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import beans.EC;
 import beans.Enseignant;
 import beans.Salle;
-import dao.DAO;
 import dao.ECDAO;
 import dao.EnseignantDAO;
 import dao.SalleDAO;
@@ -36,18 +36,18 @@ public class ModeleCreaneau {
 	}
 
 	
-	public static ArrayList<Enseignant> getAllEnseignants(){
+	public static List<Enseignant> getAllEnseignants(){
 		
 		EnseignantDAO daoEns= new EnseignantDAO(); 
 		return (ArrayList<Enseignant>) daoEns.findAll();
 	}
-	public static ArrayList<Salle> getAllSalle(){
+	public static List<Salle> getAllSalle(){
 		
 		SalleDAO daoSalle= new SalleDAO(); 
 		return (ArrayList<Salle>) daoSalle.findAll();
 	}
 	
-	public static ArrayList<EC> getAllEC(){
+	public static List<EC> getAllEC(){
 		
 		ECDAO daoEc= new ECDAO(); 
 		return (ArrayList<EC>) daoEc.findAll();
