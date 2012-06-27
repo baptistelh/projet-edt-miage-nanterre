eb43d8e5e892a8a9f7bac9b5bdffb45a3ba79cbb
baptiste.lehouarner@gmail.com <baptiste.lehouarner@gmail.com@638a33bc-f929-9441-b019-cb0cb3681f00>
2012-06-27 17:00:00 +0000

    Correction erreurs Changement nom classe par rapport aux normes de nomages Java

---
+++ src/beansUtilisation/VerificationCreneauNouveau.java
@@ -0,0 +1,51 @@
+package beansUtilisation;
+
+import java.util.Enumeration;
+import java.util.GregorianCalendar;
+import java.util.Hashtable;
+import java.util.List;
+
+import beans.Creneau;
+import beans.Jours;
+import beans.Promotion;
+import beans.Salle;
+
+public class VerificationCreneauNouveau {
+
+	
+	public static Boolean HeureMaxSemaine(List<Creneau> c){
+		Boolean r = false;
+		
+		// Hashtable pour stocker duree des journées
+		Hashtable<GregorianCalendar, Integer> dureesJournees = new Hashtable<GregorianCalendar, Integer>();
+
+		// parcours jours et addition des durées
+		for (Creneau tempC : c) {
+			// recuperation date du creneau
+			GregorianCalendar g = tempC.getDateCreneau().getDateDuJour();
+			
+			// recuperation et addition durée
+			int duree = dureesJournees.get(g);
+			duree += tempC.getDuree();
+			
+			// Mise à jour dans la hashtable
+			dureesJournees.put(g, duree);
+		}
+
+		// verification dépassement par semaine en parcourant la hashtable
+		Enumeration<GregorianCalendar> enu = dureesJournees.keys();
+		while (enu.hasMoreElements()) {
+			GregorianCalendar o = enu.nextElement();
+			Integer dureeJournee = dureesJournees.get(o);
+			if (dureeJournee > 480) {
+				r = false;
+				// throw new
+				// HorairesException("journne depassant 8h" + o.toString());
+			}
+		}
+		
+		return r;
+		
+		
+	}
+}
