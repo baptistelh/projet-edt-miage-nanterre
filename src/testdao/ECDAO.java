3d0e5120ecbb6ffec000df03eb450568f69fa60a
baptiste.lehouarner@gmail.com <baptiste.lehouarner@gmail.com@638a33bc-f929-9441-b019-cb0cb3681f00>
2012-06-27 17:08:50 +0000

    Changement nom package par rapport aux normes de nomages Java

---
+++ src/testdao/ECDAO.java
@@ -0,0 +1,67 @@
+package testdao;
+import java.util.ArrayList;
+import java.util.List;
+
+import beans.*;
+
+
+public class ECDAO {
+	
+	private static List<EC> list = new ArrayList<EC>();
+
+	public EC find(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public EC find(int num_ec, int num_ue, int num_formation) {
+		for(EC e : ECDAO.list) {
+			if(e.getNumeroEC() == num_ec)
+				return e;
+		}
+		return null;
+	}
+
+	public List<EC> findAll() {
+	    return ECDAO.list;
+	}
+	
+	public EC create(EC obj) {
+		ECDAO.list.add(obj);
+		return obj;
+	}
+
+	public EC update(EC obj) {
+		for(EC e : ECDAO.list) {
+			if(e.getNumeroEC() == obj.getNumeroEC())
+				ECDAO.list.remove(e);
+		}
+		ECDAO.list.add(obj);
+		return obj;
+	}
+
+	public void delete(EC obj) {
+		ECDAO.list.remove(obj);
+	}
+	
+	public void loadMesExamens(EC obj) {
+
+	}
+	
+	public void loadMesNecessites(EC obj) {
+
+	}
+	
+	public void loadMesVoeuxEC(EC obj) {
+
+	}
+	
+	public void loadMesCreneaux(EC obj) {
+
+	}
+	
+	public void loadMesServices(EC obj){
+
+	}
+
+}
