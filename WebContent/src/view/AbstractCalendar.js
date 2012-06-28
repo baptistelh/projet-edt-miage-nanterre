a892f6c91b2c20361bc3ee5af9305b1c290336af
baptiste.lehouarner@gmail.com <baptiste.lehouarner@gmail.com@638a33bc-f929-9441-b019-cb0cb3681f00>
2012-06-28 21:52:48 +0000

    Modification vue calendrier 1 semaine = 7jours, premier jour lundi Modification jeux de test WebService

--- WebContent/src/view/AbstractCalendar.js
+++ WebContent/src/view/AbstractCalendar.js
@@ -17,7 +17,7 @@ Ext.define('Ext.calendar.view.AbstractCalendar', {
      * @cfg {Number} startDay
      * The 0-based index for the day on which the calendar week begins (0=Sunday, which is the default)
      */
-    startDay: 2,
+    startDay: 1,
     /**
      * @cfg {Boolean} spansHavePriority
      * Allows switching between two different modes of rendering events that span multiple days. When true,
