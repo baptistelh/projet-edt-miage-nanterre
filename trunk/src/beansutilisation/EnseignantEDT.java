c7fbcc788f2c6707baaf241da7efd20173890665
onur.mia@gmail.com <onur.mia@gmail.com@638a33bc-f929-9441-b019-cb0cb3681f00>
2012-06-28 08:32:41 +0000

    Merge modifications

--- trunk/src/beansutilisation/EnseignantEDT.java
+++ trunk/src/beansutilisation/EnseignantEDT.java
@@ -2,70 +2,73 @@ package beansutilisation;
 
 import beans.*;
 
+import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.GregorianCalendar;
+import java.util.Hashtable;
 import java.util.List;
-
-import testdao.CreneauDAO;
 
 public class EnseignantEDT {
 
-	public void setCreneau(List<Creneau> newCreneaux, Enseignant e) {
+	public static void TestEnseignant(List<Creneau> newCreneaux, Enseignant e)
+			throws VerificationException {
 
-		// Connection à la base et récupération de tous les créneaux de
-		// l'enseignant en paramètre
-//		String login = "";
-//		String password = "";
-//		String url = "";
-//		Connection connection = ODBC_onur.getConnectiontoDB(login, password,
-//				url);
-		CreneauDAO creneauDAO = new CreneauDAO();
-		List<Creneau> creneauxRegistred = creneauDAO.findAll();
+		if (!EnseignantEDT.respectVoeuxEnseignant(newCreneaux, e))
+			throw new VerificationException("voeux enseignants non respectes");
 
-		// Comparaison chevauchement creneaux;
-		boolean chevauche = VerificationCreneaux.chevauchementCreneaux(
-				creneauxRegistred, newCreneaux);
-		List<Creneau> merge = creneauxRegistred;
-		merge.addAll(newCreneaux);
-
-		boolean depassementjournee = VerificationCreneaux
-				.depassementJournee(merge);
-		boolean voeux = respectVoeuxEnseignant(merge, e);
+		if (!VerificationCreneaux.chevauchementCreneaux(newCreneaux,
+				e.getMesCreneaux()))
+			throw new VerificationException("le nouveau creneau pour cet enseignant est deja alloue");
 		
-		if(chevauche && depassementjournee && voeux){
-			e.setMesCreneaux(merge);
-//			creneauDAO.create(obj);
-		}
+		
+		//Creation d'une liste mergeant les anciens crenaux et le nouveau recu
+		
+		ArrayList<Creneau> crenToTest = new ArrayList<Creneau>();
+		
+		crenToTest.addAll(newCreneaux);
+		crenToTest.addAll(e.getMesCreneaux());
+		if (!VerificationCreneaux.depassementJournee(crenToTest))
+			throw new VerificationException("L'enseignant a déjà un nombre d'heure suffisant pour la semaine");
+
 	}
 
-	public static boolean respectVoeuxEnseignant(List<Creneau> creneaux, Enseignant e){
-		boolean result=true;
-		List <Indisponibilite> indispos =e.getMesIndisponibilites();
-		List <Creneau> indisposC = new ArrayList<Creneau>();
+	public static boolean respectVoeuxEnseignant(List<Creneau> creneaux,
+			Enseignant e) {
+		boolean result = true;
+		List<Indisponibilite> indispos = e.getMesIndisponibilites();
+		List<Creneau> indisposC = new ArrayList<Creneau>();
 		String horaireIndispo;
-		Enseignant tempEns=e;
-		Salle tempSalle=new Salle();
-		EC tempEC =new EC();
-		Type tempType=new Type();
-		// On parcours la liste des indispos. On garde que les indispos ou poids=0 et on crée une liste de creneaux les contenant pour comparaison avec creneau de l'enseigant (souhaité et deja enregistrés)
-		for (Indisponibilite i:indispos){
-			if (i.getPoids()!=0){
+		Enseignant tempEns = e;
+		Salle tempSalle = new Salle();
+		EC tempEC = new EC();
+		Type tempType = new Type();
+		// On parcours la liste des indispos. On garde que les indispos ou
+		// poids=0 et on crée une liste de creneaux les contenant pour
+		// comparaison avec creneau de l'enseigant (souhaité et deja
+		// enregistrés)
+		for (Indisponibilite i : indispos) {
+			if (i.getPoids() != 0) {
 				indispos.remove(i);
-			}
-			else{
-				//si l'indispo concerne le matin --> creneau genere= horaire: 08:30, sinon 13:45 et duree: 4h(240mn) .
-				if (i.getDemiJournee()==0){
-					horaireIndispo="08:30";
-				}else{
-					horaireIndispo="13:45";
+			} else {
+				// si l'indispo concerne le matin --> creneau genere= horaire:
+				// 08:30, sinon 13:45 et duree: 4h(240mn) .
+				if (i.getDemiJournee() == 0) {
+					horaireIndispo = "08:30";
+				} else {
+					horaireIndispo = "13:45";
 				}
-				Creneau temp= new Creneau(tempEns,tempSalle,tempEC,tempType,i.getDateIndisponibilite(),horaireIndispo,240);
+				Creneau temp = new Creneau(tempEns, tempSalle, tempEC,
+						tempType, i.getDateIndisponibilite(), horaireIndispo,
+						240);
 				indisposC.add(temp);
-				
+
 			}
 		}
-		
-		result=VerificationCreneaux.chevauchementCreneaux(indisposC, e.getMesCreneaux());
-		
+
+		result = VerificationCreneaux
+				.chevauchementCreneaux(indisposC, creneaux);
+
 		return result;
 	}
 }
