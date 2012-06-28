cfb085adc28e4c9a998de2c06eebce93667c1899
vinchenet@gmail.com <vinchenet@gmail.com@638a33bc-f929-9441-b019-cb0cb3681f00>
2012-06-28 15:46:13 +0000

    Correction des violations

--- trunk/src/utilitaire/Test.java
+++ trunk/src/utilitaire/Test.java
@@ -5,14 +5,16 @@ import java.util.List;
 
 
 public class Test {
+	private Test() {
+		// TODO Auto-generated constructor stub
+	}
+	
 	public static void main(String[] args) {
 		GenererJourFeries gen = new GenererJourFeries();
 		SimpleDateFormat si = new SimpleDateFormat(("dd/MM/yyyy"));
 		List<GregorianCalendar> joursFeries = gen.genererJoursFeries();
         for(GregorianCalendar g : joursFeries) {
-        	System.out.println(si.format(g.getTime()));
-        }
-		
+        	System.out.print(si.format(g.getTime()));
+        }	
 	}
-
 }
