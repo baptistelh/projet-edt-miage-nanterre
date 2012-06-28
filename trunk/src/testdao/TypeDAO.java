c63e5a4e6697937b9ede5188c80c978c2e59022b
flegaludec@gmail.com <flegaludec@gmail.com@638a33bc-f929-9441-b019-cb0cb3681f00>
2012-06-28 08:35:12 +0000

    Ajout des bouchons FormationDAO et TypeDAO

---
+++ trunk/src/testdao/TypeDAO.java
@@ -0,0 +1,46 @@
+package testdao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import beans.*;
+
+public class TypeDAO {
+	
+	private static List<Type> list = new ArrayList<Type>();
+
+	public Type create(Type obj) {
+		TypeDAO.list.add(obj);
+		return obj;
+	}
+
+	public void delete(Type obj) {
+		for(Type t : TypeDAO.list) {
+			if(t.getNumeroType() == obj.getNumeroType())
+				TypeDAO.list.remove(t);
+		}
+		TypeDAO.list.add(obj);
+	}
+
+	public Type find(int obj) {
+		for(Type t : TypeDAO.list) {
+			if(t.getNumeroType() == obj)
+				return t;
+		}
+		return null;
+	}
+
+	public Type update(Type obj) {
+		for(Type t : TypeDAO.list) {
+			if(t.getNumeroType() == obj.getNumeroType())
+				TypeDAO.list.remove(t);
+		}
+		TypeDAO.list.add(obj);
+		return obj;
+	}
+
+	public List<Type> findAll() {
+		return TypeDAO.list;
+	}
+
+}
