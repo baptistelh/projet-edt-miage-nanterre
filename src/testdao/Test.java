3b6015242f7c66031b274ffdb275b33d1961390d
guill.delisle@gmail.com <guill.delisle@gmail.com@638a33bc-f929-9441-b019-cb0cb3681f00>
2012-06-28 08:52:44 +0000

    Mise en place du main Test pour alimenter les listes de toutes les dao

--- src/testdao/Test.java
+++ src/testdao/Test.java
@@ -4,6 +4,10 @@ public class Test {
 	
 	public static void main(String[] args) {
 		//Instanciation du DAO de la salle
-			
+			EnseignantDAO.loadMesEnseignants();
+			SalleDAO.loadMesSalles();
+			ECDAO.loadMesEC();
+			FormationDAO.loadMesFormations();
+			TypeDAO.loadMesFormations();
 	}
 }
