d3a58351d4095f2f5ef8d94145a975ff95164494
flegaludec@gmail.com <flegaludec@gmail.com@638a33bc-f929-9441-b019-cb0cb3681f00>
2012-06-29 08:13:43 +0000

    modifications pour le fonctionnement du web service dans le modele

--- trunk/src/beansutilisation/ModeleCreneau.java
+++ trunk/src/beansutilisation/ModeleCreneau.java
@@ -29,9 +29,9 @@ public final class ModeleCreneau {
 	private ModeleCreneau() {
 		// TODO Auto-generated constructor stub
 	}
-	
-	public static boolean creation(int idEns, String idSalle, int idEc, int typearg,
-			int idPromo, String date, String horaire, int duree) {
+
+	public static boolean creation(int idEns, String idSalle, int idEc,
+			int typearg, int idPromo, String date, String horaire, int duree) {
 
 		EnseignantDAO ensDAO = new EnseignantDAO();
 		Enseignant ens = ensDAO.find(idEns);
@@ -48,49 +48,52 @@ public final class ModeleCreneau {
 		PromotionDAO promDAO = new PromotionDAO();
 		Promotion prom = promDAO.find(idPromo);
 
-		int d = Integer.parseInt(date.substring(0, 2));
-		int m = Integer.parseInt(date.substring(3, 5));
-		int y = Integer.parseInt(date.substring(6));
-		GregorianCalendar gcDate = new GregorianCalendar(y, m-1, d);
+		int d = Integer.parseInt(date.split("/")[0]);
+		int m = Integer.parseInt(date.split("/")[1]);
+		int y = Integer.parseInt(date.split("/")[2]);
 
-		JoursDAO jourDAO = new JoursDAO();
-		Jours jour = jourDAO.find(gcDate);
+		GregorianCalendar gcDate = new GregorianCalendar(y, m - 1, d);
+
+		System.out.println(y+" "+ m+" "+ d);
+		
+//		JoursDAO jourDAO = new JoursDAO();
+//		Jours jour = jourDAO.find(gcDate);
+		Jours jour = new Jours(gcDate);
+		
 
 		// Creation d'un bean creneau selon les arguments re�us
 		Creneau newCreneau = new Creneau(ens, salle, ec, type, jour, horaire,
 				duree);
-		ArrayList<Creneau> newCreneaux = new ArrayList<Creneau>();
-		newCreneaux.add(newCreneau);
 
-//		try {
-//			EnseignantEDT.TestEnseignant(newCreneaux, ens);
-//			VerificationPromoSalle.verifSalle(newCreneaux, salle);
-//			VerificationPromoSalle.verifPromo(newCreneaux, prom);
-//			VerificationEC.verificationTempsEC(ec, type);
-//		} catch (VerificationException e) {
-//			System.err.println(e.getMessage());
-//			return false;
-//		}
+		// try {
+		// EnseignantEDT.testEnseignant(newCreneaux, ens);
+		// VerificationPromoSalle.verifSalle(newCreneaux, salle);
+		// VerificationPromoSalle.verifPromo(newCreneaux, prom);
+		// VerificationEC.verificationTempsEC(ec, type);
+		// } catch (VerificationException e) {
+		// System.err.println(e.getMessage());
+		// return false;
+		// }
 
 		// cr�ation du cr�neau
 		CreneauDAO creDao = new CreneauDAO();
 		creDao.create(newCreneau);
 
-//		// update de l'enseignant - PEUT ETRE A ENLEVER CAR PAS DE CHANGEMENT
-//		// NORMALEMENT
-//		ArrayList<Creneau> newCreneauxEns = new ArrayList<Creneau>();
-//		newCreneauxEns.addAll(ens.getMesCreneaux());
-//		newCreneauxEns.addAll(newCreneaux);
-//		ens.setMesCreneaux(newCreneauxEns);
-//		ensDAO.update(ens);
-//
-//		// update de la salle PEUT ETRE A ENLEVER CAR PAS DE CHANGEMENT
-//		// NORMALEMENT
-//		ArrayList<Creneau> newCreneauxsalle = new ArrayList<Creneau>();
-//		newCreneauxsalle.addAll(salle.getMesCreneaux());
-//		newCreneauxsalle.addAll(newCreneaux);
-//		salle.setMesCreneaux(newCreneauxsalle);
-//		salleDAO.update(salle);
+		// // update de l'enseignant - PEUT ETRE A ENLEVER CAR PAS DE CHANGEMENT
+		// // NORMALEMENT
+		// ArrayList<Creneau> newCreneauxEns = new ArrayList<Creneau>();
+		// newCreneauxEns.addAll(ens.getMesCreneaux());
+		// newCreneauxEns.addAll(newCreneaux);
+		// ens.setMesCreneaux(newCreneauxEns);
+		// ensDAO.update(ens);
+		//
+		// // update de la salle PEUT ETRE A ENLEVER CAR PAS DE CHANGEMENT
+		// // NORMALEMENT
+		// ArrayList<Creneau> newCreneauxsalle = new ArrayList<Creneau>();
+		// newCreneauxsalle.addAll(salle.getMesCreneaux());
+		// newCreneauxsalle.addAll(newCreneaux);
+		// salle.setMesCreneaux(newCreneauxsalle);
+		// salleDAO.update(salle);
 		return true;
 
 	}
@@ -123,7 +126,7 @@ public final class ModeleCreneau {
 		int d = Integer.parseInt(date.substring(0, 2));
 		int m = Integer.parseInt(date.substring(3, 5));
 		int y = Integer.parseInt(date.substring(6));
-		GregorianCalendar gcDate = new GregorianCalendar(y, m-1, d);
+		GregorianCalendar gcDate = new GregorianCalendar(y, m - 1, d);
 
 		JoursDAO jourDAO = new JoursDAO();
 		Jours jour = jourDAO.find(gcDate);
@@ -179,7 +182,7 @@ public final class ModeleCreneau {
 		int d = Integer.parseInt(date.substring(0, 2));
 		int m = Integer.parseInt(date.substring(3, 5));
 		int y = Integer.parseInt(date.substring(6));
-		GregorianCalendar gcDate = new GregorianCalendar(y, m-1, d);
+		GregorianCalendar gcDate = new GregorianCalendar(y, m - 1, d);
 
 		JoursDAO jourDAO = new JoursDAO();
 		Jours jour = jourDAO.find(gcDate);
@@ -197,62 +200,60 @@ public final class ModeleCreneau {
 	}
 
 	public static Creneau getCreneau(int noEnseignant, String noSalle,
-			int noEc, int noType,
-			String date) {
-		
+			int noEc, int noType, String date) {
+
 		ECDAO.loadMesEC();
-		
+
 		int d = Integer.parseInt(date.substring(0, 2));
 		int m = Integer.parseInt(date.substring(3, 5));
 		int y = Integer.parseInt(date.substring(6));
-		GregorianCalendar gcDate = new GregorianCalendar(y, m-1, d);
-		
+		GregorianCalendar gcDate = new GregorianCalendar(y, m - 1, d);
+
 		ECDAO ecDAO = new ECDAO();
 		EC ec = ecDAO.find(noEc);
-		
-		
+
 		CreneauDAO creDao = new CreneauDAO();
-		return creDao.find(noEnseignant, noSalle, noEc, ec.getMonUE().getNumeroUE(), ec.getMonUE().getMaFormation().getNumeroFormation(),
-				noType, gcDate);
+		return creDao.find(noEnseignant, noSalle, noEc, ec.getMonUE()
+				.getNumeroUE(), ec.getMonUE().getMaFormation()
+				.getNumeroFormation(), noType, gcDate);
 
 	}
 
 	public static List<Enseignant> getAllEnseignants() {
 		EnseignantDAO daoEns = new EnseignantDAO();
-		if(daoEns.findAll().isEmpty())
+		if (daoEns.findAll().isEmpty())
 			EnseignantDAO.loadMesEnseignants();
 		return daoEns.findAll();
 	}
 
 	public static List<Salle> getAllSalle() {
 		SalleDAO daoSalle = new SalleDAO();
-		if(daoSalle.findAll().isEmpty())
+		if (daoSalle.findAll().isEmpty())
 			SalleDAO.loadMesSalles();
 		return daoSalle.findAll();
 	}
 
 	public static List<EC> getAllEC() {
 		ECDAO daoEc = new ECDAO();
-		if(daoEc.findAll().isEmpty())
+		if (daoEc.findAll().isEmpty())
 			ECDAO.loadMesEC();
 		return daoEc.findAll();
 	}
 
 	public static List<Type> getAllTypes() {
 		TypeDAO daoType = new TypeDAO();
-		if(daoType.findAll().isEmpty())
+		if (daoType.findAll().isEmpty())
 			TypeDAO.loadMesTypes();
 		return daoType.findAll();
 	}
 
 	public static List<Formation> getAllFormation() {
 		FormationDAO daoFormation = new FormationDAO();
-		if(daoFormation.findAll().isEmpty())
+		if (daoFormation.findAll().isEmpty())
 			FormationDAO.loadMesFormations();
 
-		
-//		List<Formation> formations = (ArrayList<Formation>) daoFormation
-//				.findAll();
+		// List<Formation> formations = (ArrayList<Formation>) daoFormation
+		// .findAll();
 		return daoFormation.findAll();
 	}
 }
